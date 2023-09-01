e580b0d6710835a0288eddd2d133a2eb7ce047fa
JamesC215 <carter215@hotmail.co.uk>
2023-09-01 13:44:51 +0100

    change server.js

--- server.js
+++ server.js
@@ -14,13 +14,6 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-08-01",
 });
 
-app.use(express.static(process.env.STATIC_DIR));
-
-app.get("/", (req, res) => {
-  const path = resolve(process.env.STATIC_DIR + "/index.html");
-  res.sendFile(path);
-});
-
 app.get("/config", (req, res) => {
   res.send({
     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
